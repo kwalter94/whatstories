d2efbcd7c28823fa914a008012082212cd530553
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2020-04-08 22:42:36 +0200

    Add gifencoder, write to GIF
    incorporated gifencoder - progress!

--- whatstories.js
+++ whatstories.js
@@ -1,8 +1,11 @@
-const process = require('process');
-
+const GIFEncoder = require('gifencoder');
+const { createCanvas } = require('canvas');
+const fs = require('fs');
+ 
 const WORDS_PER_MIN = {
     100: '100 WPM - Lazy',
     200: '100 WPM - Could do better',
+    266: '100 WPM - Could do better',
     300: '300 WPM - Now we reading',
     400: '400 WPM - Kinda Fast',
     500: '500 WPM - Damn, yo',
@@ -18,17 +21,30 @@ const WORDS_PER_MIN = {
  * @return int the value representing animation rate 
  */
 function wpmToAnimationIntervalMillis(wpmValue) {
-    // 500 words per minute == 500 / 60 seconds =
-    return Math.round(60000 / wpmValue);
+    return Math.floor(60000 / wpmValue);
 }
 
-function startStory(text, wordsPerMin) {
+function createStory(text, wordsPerMin, width, height, filename) {
     var corpus = text.split(' ').filter(val => val && val.length > 0);
     var curIndex = 0;
     var previousWord = '';
     var currentWord =  '';
     const animationRate = wpmToAnimationIntervalMillis(wordsPerMin);
-    var intervalVal = setInterval(() => {
+
+    const encoder = new GIFEncoder(width, height);
+    // stream the results as they are available into myanimated.gif
+    encoder.createReadStream().pipe(fs.createWriteStream(filename));
+     
+    encoder.start();
+    encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+    encoder.setDelay(animationRate);  // frame delay in ms
+    encoder.setQuality(10); // image quality. 10 is default.
+
+    // use node-canvas
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+    
+    for(var curIndex = 0; curIndex < corpus.length; curIndex++) {
         currentWord = corpus[curIndex];
         empty = "";
         if (curIndex > 0 && curIndex < corpus.length) {
@@ -37,14 +53,29 @@ function startStory(text, wordsPerMin) {
                 empty = " ".repeat(previousWord.length - currentWord.length); 
             }
         }
-        process.stdout.write(`\r${currentWord}${empty}`)
-        curIndex++;
+        // process.stdout.write(`\r${currentWord}${empty}`)
+        
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, width, height);
+        ctx.fillStyle = '#ffffff';
+        ctx.font = '50px Arial';
+        // TODO: Put word in the center of the image..
+        ctx.fillText(currentWord, width/2 - 100, height/2);
+        encoder.addFrame(ctx);
+    }
+    // 10 extra frames for a "pause"
+    for(var i = 0; i < 10; i++) {
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, width, height);
+        encoder.addFrame(ctx);
+    }
 
-        if (curIndex == corpus.length) {
-            clearInterval(intervalVal);
-        }
-    }, animationRate);
+    encoder.finish();
 }
 
+// createStory('Hello this is a test of the the wpm to animation interval function', 300, 500, 500, 'file.gif');
+createStory("Hello there, this is a basic story created by WhatStories. You are currently reading this at 100 words per minute- learn to read faster!", 100, 500, 500, 'file-100.gif');
 
-startStory('Hello this is a test of the the wpm to animation interval function', 300);
+createStory("Hello there, this is a basic story created by WhatStories. You are currently reading this at 200 words per minute- so if you're able to follow along you're okay at reading, but could be faster. ", 100, 500, 500, 'file-200.gif');
+
+createStory("Hello there, this is a basic story created by WhatStories. You are currently reading this at 300 words per minute- so if you're able to follow along you're a pretty fast reader. ", 300, 500, 500, 'file-300.gif');
